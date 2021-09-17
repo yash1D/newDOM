5f06131a4be6029301753f119fc948b035dd82a7
yash1D <90711974+yash1D@users.noreply.github.com>
2021-09-17 20:30:03 +0530

    newtask

---
+++ newDOM/newDOMtask.js
@@ -0,0 +1,12 @@
+ 
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header')
+console.log(headerTitle);
+headerTitle.innerText='HELLO';
+headerTitle.textContent ='GOODNIGHT'
+
+var model =document.getElementsByClassName('title')
+console.log(model);
+console.log(model[0]);
+model[0].style.fontWeight ='bold';
+model[0].style.backgroundcolor ='red';
